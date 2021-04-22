5d0200a256eee7068aee24d1fde39a5acc1648f8
Your Name <you@example.com>
2021-04-23 04:00:57 +0530

    Firebase Authentication and Accounts Screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
 
 import {
@@ -10,11 +10,24 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
 
-import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
-import { LocationContextProvider } from "./src/services/location/location.context";
-import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
 
 import { Navigation } from "./src/infrastructure/navigation";
+
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDq7J-PENJG_cH_qYM7J5OIOgVkl50m_uA",
+  authDomain: "mealstogo-dba80.firebaseapp.com",
+  projectId: "mealstogo-dba80",
+  storageBucket: "mealstogo-dba80.appspot.com",
+  messagingSenderId: "330238219629",
+  appId: "1:330238219629:web:c337a55d7dff1a52cb7664",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -27,16 +40,13 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
